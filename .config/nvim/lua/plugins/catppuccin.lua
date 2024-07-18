13961a9bbf92b1cffa652f41c5d991c0354236b7
Timothy Besada <tibes.91@gmail.com>
2024-07-18 03:28:01 +0200

    cleanup such that only changed options remain

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,9 +1,6 @@
 return {
   "catppuccin/nvim",
   name = "catppuccin",
-  -- init = function()
-  --   vim.opt.winblend = 50
-  -- end,
   opts = {
     term_colors = true,
     transparent_background = true,
@@ -15,41 +12,6 @@ return {
         LspReferenceWrite = { underline = true, bg = "" },
       }
     end,
-    integrations = {
-      aerial = true,
-      alpha = true,
-      cmp = true,
-      dashboard = true,
-      flash = true,
-      gitsigns = true,
-      headlines = true,
-      illuminate = true,
-      indent_blankline = { enabled = true },
-      leap = true,
-      lsp_trouble = true,
-      mason = true,
-      markdown = true,
-      mini = true,
-      native_lsp = {
-        enabled = true,
-        underlines = {
-          errors = { "undercurl" },
-          hints = { "undercurl" },
-          warnings = { "undercurl" },
-          information = { "undercurl" },
-        },
-      },
-      navic = { enabled = true, custom_bg = "lualine" },
-      neotest = true,
-      neotree = true,
-      noice = true,
-      notify = true,
-      semantic_tokens = true,
-      telescope = true,
-      treesitter = true,
-      treesitter_context = true,
-      which_key = true,
-    },
     styles = {
       sidebars = "transparent",
       floats = "transparent",
